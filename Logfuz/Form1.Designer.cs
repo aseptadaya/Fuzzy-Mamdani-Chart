a1ef5152a7dcab270c997fff03dd6325f05cb0ba
aseptadaya <aseptadaya@null.net>
2016-11-29 08:46:12 +0700

    Add project files.

---
+++ Logfuz/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿namespace Logfuz
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.textA = new System.Windows.Forms.TextBox();
+            this.textB = new System.Windows.Forms.TextBox();
+            this.textC = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.labelInterval = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.chart1);
+            this.panel1.Location = new System.Drawing.Point(203, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(420, 269);
+            this.panel1.TabIndex = 0;
+            // 
+            // textA
+            // 
+            this.textA.Location = new System.Drawing.Point(70, 33);
+            this.textA.Name = "textA";
+            this.textA.Size = new System.Drawing.Size(100, 20);
+            this.textA.TabIndex = 0;
+            this.textA.TextChanged += new System.EventHandler(this.textA_TextChanged);
+            // 
+            // textB
+            // 
+            this.textB.Location = new System.Drawing.Point(70, 75);
+            this.textB.Name = "textB";
+            this.textB.Size = new System.Drawing.Size(100, 20);
+            this.textB.TabIndex = 1;
+            // 
+            // textC
+            // 
+            this.textC.Location = new System.Drawing.Point(70, 120);
+            this.textC.Name = "textC";
+            this.textC.Size = new System.Drawing.Size(100, 20);
+            this.textC.TabIndex = 2;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(70, 163);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(8, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(14, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "B";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(8, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(14, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "C";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(8, 166);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "alpha";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(11, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Proses";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // labelInterval
+            // 
+            this.labelInterval.AutoSize = true;
+            this.labelInterval.Location = new System.Drawing.Point(106, 232);
+            this.labelInterval.Name = "labelInterval";
+            this.labelInterval.Size = new System.Drawing.Size(34, 13);
+            this.labelInterval.TabIndex = 9;
+            this.labelInterval.Text = "[_   _]";
+            this.labelInterval.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(106, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Interval";
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(4, 4);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series1.CustomProperties = "IsXAxisQuantitative=False";
+            series1.Legend = "Legend1";
+            series1.Name = "Fuzzy Number";
+            series1.XValueMember = "X";
+            series1.YValueMembers = "Y";
+            series2.ChartArea = "ChartArea1";
+            series2.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Line;
+            series2.EmptyPointStyle.BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            series2.LabelBorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            series2.Legend = "Legend1";
+            series2.Name = "alpha Cut";
+            series2.SmartLabelStyle.CalloutLineDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dot;
+            this.chart1.Series.Add(series1);
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(413, 262);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            this.chart1.Click += new System.EventHandler(this.chart1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 293);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.labelInterval);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textA);
+            this.Controls.Add(this.textB);
+            this.Controls.Add(this.textC);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textA;
+        private System.Windows.Forms.TextBox textB;
+        private System.Windows.Forms.TextBox textC;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label labelInterval;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+    }
+}
+
---
+++ Logfuz/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Logfuz
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int aa, bb, cc;
+            double alpha;
+            aa = int.Parse(textA.Text);
+            bb = int.Parse(textB.Text);
+            cc = int.Parse(textC.Text);
+            try
+            {
+                double result;
+                if( double.TryParse(textBox4.Text,out result))
+                {
+                    alpha = double.Parse(textBox4.Text);
+                }
+                else
+                {
+                    alpha = 0;
+                }
+                
+            }
+            catch (Exception)
+            {
+                alpha = 0;
+                throw;
+            }
+            
+
+            //Delete all chart
+            chart1.Series["Fuzzy Number"].Points.Clear();
+            chart1.Series["alpha Cut"].Points.Clear();
+
+
+
+            if (alpha == 0)
+            {
+                chart1.Series["Fuzzy Number"].Points.AddXY(aa, 0);
+                chart1.Series["Fuzzy Number"].Points.AddXY(bb, 1);
+                chart1.Series["Fuzzy Number"].Points.AddXY(cc, 0);
+                labelInterval.Text = "[_   _]";
+            }
+            else
+            {
+                chart1.Series["Fuzzy Number"].Points.AddXY(aa, 0);
+                chart1.Series["Fuzzy Number"].Points.AddXY(bb, 1);
+                chart1.Series["Fuzzy Number"].Points.AddXY(cc, 0);
+                double x1, x3;
+                x1 = alpha * (bb - aa) + aa;
+                x3 = -1 * (alpha * (cc - bb)) + cc;
+                Console.WriteLine(x1);
+                Console.WriteLine(x3);
+                //chart1.Series["alpha Cut"].Points.AddXY(x1, 0);
+                chart1.Series["alpha Cut"].Points.AddXY(x1, alpha);
+                chart1.Series["alpha Cut"].Points.AddXY(x3, alpha);
+                //chart1.Series["alpha Cut"].Points.AddXY(x3, 0);
+                labelInterval.Text = "["+x1+"   "+x3+"]";
+            }
+			chart1.Series["Fuzzy Number"].BorderWidth = 4;
+            chart1.Series["Fuzzy Number"].Color = Color.Black;
+            chart1.Series["alpha Cut"].BorderWidth = 2;
+            chart1.Series["alpha Cut"].Color = Color.Red;
+            chart1.Series["alpha Cut"].BorderDashStyle = System.Windows.Forms.DataVisualization.Charting.ChartDashStyle.Dash;
+
+
+            
+            
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textA_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
